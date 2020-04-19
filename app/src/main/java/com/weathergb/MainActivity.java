8b5584974c674888386ce2b5354e6395913ee5b9
Alex Podgurski <alexbox726@gmail.com>
2020-04-19 18:00:54 +0300

    Стабильная/Чистая версия

--- app/src/main/java/com/weathergb/MainActivity.java
+++ app/src/main/java/com/weathergb/MainActivity.java
@@ -32,41 +32,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pLogick();
-        Toaster("onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Toaster("onStart");
     }
 
     // Возвращаемся
     @Override
     protected void onResume() {
         super.onResume();
-        Toaster("onResume");
     }
 
     // Пауза
     @Override
     protected void onPause() {
         super.onPause();
-        Toaster("onPause");
     }
 
     // Перезапуск
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toaster("onRestart");
     }
 
     // Восстановление
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        Toaster("onRestoreInstanceState");
         currTemp = savedInstanceState.getInt(TEMPS);
     }
 
@@ -74,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        Toaster("onSaveInstanceState");
         outState.putInt(TEMPS, currTemp);
     }
 
@@ -82,7 +75,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        Toaster("onStop");
     }
 
     // Смэрть
@@ -100,59 +92,21 @@ public class MainActivity extends AppCompatActivity {
 
     // Логика
     @SuppressLint("SetTextI18n")
-    public void pLogick() {
-        // Выбранная страна в данный момент
-        String currCountry;
-        // Выбранный город
+    protected void pLogick() {
         String cityS;
-        char stateP = '+';
-        char stateM = '-';
-        // Тип температуры, значение будет меняться в настройках
-        char TempType = 'C';
-        // Три дня вперёд
-        String FirstD, SecondD, ThirdD;
-        // Температура на дни
-        int FirstN, SecondN, ThridN;
+
 
         TextView cel = findViewById(R.id.textWeather);
         TextView city = findViewById(R.id.textCity);
         TextView greeting = findViewById(R.id.textStatus);
-        TextView country = findViewById(R.id.textCountry);
-        TextView past = findViewById(R.id.textPast);
-        TextView present = findViewById(R.id.textPresent);
-        TextView future = findViewById(R.id.textFuture);
-        TextView type = findViewById(R.id.textType);
-        TextView dayOne = findViewById(R.id.dayOne);
-        TextView dayTwo = findViewById(R.id.dayTwo);
-        TextView dayThree = findViewById(R.id.dayThree);
-        TextView d1 = findViewById(R.id.d1);
-        TextView d2 = findViewById(R.id.d2);
-        TextView d3 = findViewById(R.id.d3);
 
         cityS = "Москва";
-        currCountry = "Россия";
-        FirstD = "Суббота";
-        SecondD = "Воскресенье";
-        ThirdD = "Понедельник";
-        FirstN = 8;
-        SecondN = 10;
-        ThridN = 12;
-        int pTemp = 10, fTemp = 10, paTemp = 8;
 
-        cel.setText(stateP + " " + currTemp + TempType);
+
+        cel.setText(String.valueOf(currTemp));
         city.setText(cityS);
-        country.setText(currCountry);
+
         greeting.setText("Доброе утро!");
-        past.setText(stateP + " " + paTemp + " " + TempType + "\n" + "8:00");
-        present.setText(stateP + " " + pTemp + " " + TempType + "\n" + "9:00");
-        future.setText(stateP + " " + fTemp + " " + TempType + "\n" + "10:00");
-        type.setText("Ясно");
-        dayOne.setText(FirstD);
-        d1.setText(stateP + " " + FirstN + " " + TempType);
-        dayTwo.setText(SecondD);
-        d2.setText(stateP + " " + SecondN + " " + TempType);
-        dayThree.setText(ThirdD);
-        d3.setText(stateP + " " +ThridN + " " + TempType);
     }
 
     // Тестирую Toast
