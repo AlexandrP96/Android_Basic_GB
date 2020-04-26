ab32e54fca12cd8f1594d953544636bdc064c566
Alex Podgurski <alexbox726@gmail.com>
2020-04-26 18:11:22 +0300

    Lesson_6
    Добавил простой рабочий parcel и SecondActivity

--- app/src/main/java/com/weathergb/MainActivity.java
+++ app/src/main/java/com/weathergb/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,8 +14,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+// Добавил SecondActivity с адекватным парселем 
+
+
+public class MainActivity extends AppCompatActivity implements Constants {
 
     // Констната Лога
     private static final String LOG = "Activity";
@@ -29,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView currentCity = findViewById(R.id.currentCityV);
+        Parcel parcel = (Parcel) getIntent().getSerializableExtra(PARCEL);
+
+        if (parcel == null) {
+            currentCity.setText("Default");
+        } else {
+            currentCity.setText(parcel.currentCity);
+        }
 
         pLogick();
         Log.i(LOG,"onCreate");
@@ -105,27 +119,18 @@ public class MainActivity extends AppCompatActivity {
         cel.setText(currTemp + "C");
         cel2.setText(String.valueOf(currTemp));
 
-        // Устанавливаем дефолтный фрагмент (Временное)
-        DefaultFragment fragmentD = new DefaultFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.FragmentContainer, fragmentD);
-        fragmentTransaction.addToBackStack("");
-        fragmentTransaction.commit();
-
-        Log.i(LOG,"DefaultCity");
         Log.i(LOG,"AppLogic");
     }
 
 
-    // Кнопка замены фрагмента
+    // Кнопка открытия второго Активити
     public void onClick(View v) {
-        TestFragment fragmentT = new TestFragment();
-        // new ListenerOnReplace(fragmentT);
-        // Логика из урока
-        Button add1 = findViewById(R.id.buttonAdd);
-        add1.setOnClickListener(new ListenerOnReplace(fragmentT));
-        Toast.makeText(getApplicationContext(), "Фрагмент", Toast.LENGTH_SHORT).show();
-        Log.i(LOG,"onClick");
+        // Переход на SecondActivity
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        startActivity(intent);
+
+        // Toast.makeText(getApplicationContext(), "Фрагмент", Toast.LENGTH_SHORT).show();
+        Log.i(LOG,"OpenSecondActivity");
     }
 
 
@@ -135,33 +140,6 @@ public class MainActivity extends AppCompatActivity {
         Button open = findViewById(R.id.buttonQuestion);
         open.setOnClickListener(new ListenerOnAdd(cityFragment));
         Log.i(LOG,"openList");
-    }
-
-
-    // Конструктор из программы первого урока
-    class ListenerOnReplace implements View.OnClickListener {
-
-         Fragment fragment;
-
-        ListenerOnReplace(Fragment fragment) {
-           this.fragment = fragment;
-        }
-
-        @Override
-        public void onClick(View v) {
-            replaceFragment();
-        }
-
-        // Заменить фрагмент
-        private void replaceFragment() {
-            TestFragment fragmentT = new TestFragment();
-            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.FragmentContainer, fragmentT);
-            fragmentTransaction.addToBackStack("");
-            fragmentTransaction.commit();
-
-            Log.i(LOG,"replaceCity");
-        }
     }
 
 
---
+++ app/src/main/java/com/weathergb/SecondActivity.java
@@ -0,0 +1,33 @@
+package com.weathergb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity implements Constants {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+
+    public void AddButton(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.putExtra(PARCEL,createParcel());
+        startActivity(intent);
+    }
+
+
+    private Parcel createParcel(){
+        EditText SecondView = findViewById(R.id.UserText);
+        Parcel parcel = new Parcel();
+        parcel.currentCity = SecondView.getText().toString();
+        return parcel;
+    }
+}
