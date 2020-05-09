91500749ec100ba7d0296349d59089b7597ab229
Alex Podgurski <alexbox726@gmail.com>
2020-05-09 15:08:40 +0300

    Lesson_8
    Added button
    Updated Logic
    
    Humidity - doesn't work

--- app/src/main/java/com/weathergb/MainActivity.java
+++ app/src/main/java/com/weathergb/MainActivity.java
@@ -1,6 +1,7 @@
 package com.weathergb;
 
 import android.annotation.SuppressLint;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -15,7 +16,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -31,30 +31,23 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity implements Constants {
 
-    private static final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=moscow&units=celsius&appid=";
+
+    private static final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=moscow&units=metric&appid=";
     private static final String WEATHER_API_KEY = " ";
 
     private static final String LOG = "Activity";
     private static final String TEMPS = "TEMP_S";
     private static final int REQ_CODE_99 = 99;
-    // private int currTemp = 10;
-
-    private TextView Temp;
-    private TextView Pressure;
-    private TextView Wind;
-    private TextView Humidity;
-
+    private int currTemp = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // appTemp();
-        appData();
         DaysF();
         WeatherAnimation();
-        Log.i(LOG,"onCreate");
+        Log.i(LOG, "onCreate");
     }
 
 
@@ -65,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements Constants {
         if (requestCode == REQ_CODE_99) {
             if (resultCode == RESULT_OK) {
                 if (data != null) {
-                    Parcel parcel = (Parcel)data.getSerializableExtra(PARCEL);
+                    Parcel parcel = (Parcel) data.getSerializableExtra(PARCEL);
                     if (parcel != null) {
                         currentCity.setText(parcel.currentCity);
                     } else {
@@ -75,100 +68,81 @@ public class MainActivity extends AppCompatActivity implements Constants {
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
-        Log.i(LOG,"onActivityResult");
+        Log.i(LOG, "onActivityResult");
     }
 
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        //currTemp = savedInstanceState.getInt(TEMPS);
+        currTemp = savedInstanceState.getInt(TEMPS);
 
-        Log.i(LOG,"RestoreInstance");
+        Log.i(LOG, "RestoreInstance");
     }
 
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        //outState.putInt(TEMPS, currTemp);
+        outState.putInt(TEMPS, currTemp);
 
-        Log.i(LOG,"SaveInstance");
+        Log.i(LOG, "SaveInstance");
     }
 
 
-    private void appData() {
-        Temp = findViewById(R.id.textWeather);
-        Pressure = findViewById(R.id.PressureStat);
-        Wind = findViewById(R.id.WindSpeed);
-        Humidity = findViewById(R.id.HumidityStat);
-        Button rfr = findViewById(R.id.BtnRefresh);
-        rfr.setOnClickListener(click);
+    private void Thread() {
+        try {
+            final URL uri = new URL(WEATHER_URL + WEATHER_API_KEY);
+            final Handler handler = new Handler();
+            new Thread(new Runnable() {
+                @RequiresApi(api = Build.VERSION_CODES.N)
+                @Override
+                public void run() {
+                    HttpsURLConnection urlC = null;
+                    try {
+                        urlC = (HttpsURLConnection) uri.openConnection();
+                        urlC.setRequestMethod("GET");
+                        urlC.setReadTimeout(10000);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(urlC.getInputStream()));
+                        String result = getLines(in);
+                        Gson gson = new Gson();
+                        final WeatherRequest wr = gson.fromJson(result, WeatherRequest.class);
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                DisplayInfo(wr);
+                            }
+                        });
+                    } catch (Exception e) {
+                        Log.i(LOG, "Fail Connection", e);
+                        e.printStackTrace();
+                    }
+                }
+
+                @RequiresApi(api = Build.VERSION_CODES.N)
+                private String getLines(BufferedReader in) {
+                    return in.lines().collect(Collectors.joining("\n"));
+                }
+            }).start();
+
+        } catch (Exception e) {
+            Log.i(LOG, "Fail URL", e);
+            e.printStackTrace();
+        }
     }
 
 
-    private View.OnClickListener click = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            try {
-                final URL uri = new URL(WEATHER_URL + WEATHER_API_KEY);
-                final Handler handler = new Handler();
-                new Thread(new Runnable() {
-                    @RequiresApi(api = Build.VERSION_CODES.N)
-                    @Override
-                    public void run() {
-                        HttpsURLConnection urlC = null;
-                        try {
-                            urlC = (HttpsURLConnection) uri.openConnection();
-                            urlC.setRequestMethod("GET");
-                            urlC.setReadTimeout(10000);
-                            BufferedReader in = new BufferedReader(new InputStreamReader(urlC.getInputStream()));
-                            String result = getLines(in);
-                            Gson gson = new Gson();
-                            final WeatherRequest wr = gson.fromJson(result, WeatherRequest.class);
-                            handler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    DisplayInfo(wr);
-                                }
-                            });
-                        } catch (Exception e) {
-                            Log.i(LOG,"Fail Connection", e);
-                            e.printStackTrace();
-                        }
-                    }
-                    @RequiresApi(api = Build.VERSION_CODES.N)
-                    private String getLines (BufferedReader in) {
-                        return in.lines().collect(Collectors.joining("\n"));
-                    }
-                }).start();
-            } catch (Exception e) {
-                Log.i(LOG,"Fail URL", e);
-                e.printStackTrace();
-            }
-        }
-        @SuppressLint("DefaultLocale")
-        private void DisplayInfo (WeatherRequest wr) {
-            // TextView currentCity = findViewById(R.id.currentCityV);
-            // currentCity.setText(wr.getName());
-            Temp.setText(String.format("%f2", wr.getMain().getTemp()));
-            Pressure.setText(String.format("%d", wr.getMain().getPressure()));
-            Wind.setText(String.format("%d", wr.getWind().getSpeed()));
-            Humidity.setText(String.format("%d", wr.getMain().getHumidity()));
-        }
-    };
-
-
-//    @SuppressLint("SetTextI18n")
-//    protected void appTemp() {
-//        // TextView cel = findViewById(R.id.textWeather);
-//        TextView cel2 = findViewById(R.id.PresentTemp);
-//
-//        // cel.setText(currTemp + " C");
-//        cel2.setText("+ " + currTemp + " C");
-//
-//        Log.i(LOG,"AppLogic");
-//    }
+    @SuppressLint({"DefaultLocale"})
+    private void DisplayInfo(WeatherRequest wr) {
+        TextView Temp = findViewById(R.id.textWeather);
+        TextView Pressure = findViewById(R.id.PressureStat);
+        TextView Wind = findViewById(R.id.WindSpeed);
+        // TextView Humidity = findViewById(R.id.HumidityStat);
+        Temp.setText(String.format("%f2", wr.getMain().getTemp()));
+        Pressure.setText(String.format("%d", wr.getMain().getPressure()));
+        Wind.setText(String.format("%d", wr.getWind().getSpeed()));
+        // Humidity.setText(String.format("%d", wr.getMain().getHumidity()));
+    }
 
 
     protected void WeatherAnimation() {
@@ -185,14 +159,19 @@ public class MainActivity extends AppCompatActivity implements Constants {
         fragmentTransaction.replace(R.id.DaysFragment, days);
         fragmentTransaction.addToBackStack("");
         fragmentTransaction.commit();
-        Log.i(LOG,"DaysFragment");
+        Log.i(LOG, "DaysFragment");
     }
 
 
     public void onClick(View v) {
         Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
         startActivityForResult(intent, REQ_CODE_99);
+        Log.i(LOG, "OpenSecondActivity");
+    }
 
-        Log.i(LOG,"OpenSecondActivity");
+
+    public void onRefresh(View v) {
+        Thread();
+        Log.i(LOG, "Thread Launch");
     }
 }
