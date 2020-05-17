319c0ef43e3895f726dc26fdfa56a82f2cf67211
Alex Podgurski <alexbox726@gmail.com>
2020-05-17 18:11:49 +0300

    Lesson_1_1
    Добавил ButtonListener
    Убрал кнопку Refresh

--- app/src/main/java/com/weathergb/MainActivity.java
+++ app/src/main/java/com/weathergb/MainActivity.java
@@ -2,7 +2,6 @@ package com.weathergb;
 
 import android.annotation.SuppressLint;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,9 +15,9 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.weathergb.winfo.WeatherRequest;
@@ -35,9 +34,7 @@ public class MainActivity extends AppCompatActivity implements Constants {
 
 
     private static final String LOG = "Activity";
-    private static final String TEMPS = "TEMP_S";
     private static final int REQ_CODE_99 = 99;
-    private int currTemp = 10;
 
 
     @Override
@@ -52,22 +49,18 @@ public class MainActivity extends AppCompatActivity implements Constants {
     }
 
 
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        currTemp = savedInstanceState.getInt(TEMPS);
-
-        Log.i(LOG, "RestoreInstance");
-    }
-
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(TEMPS, currTemp);
-
-        Log.i(LOG, "SaveInstance");
-    }
+//    @Override
+//    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        Log.i(LOG, "RestoreInstance");
+//    }
+//
+//
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        Log.i(LOG, "SaveInstance");
+//    }
 
 
     private void Thread() {
@@ -78,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements Constants {
                 @RequiresApi(api = Build.VERSION_CODES.N)
                 @Override
                 public void run() {
-                    HttpsURLConnection urlC = null;
+                    HttpsURLConnection urlC;
                     try {
                         urlC = (HttpsURLConnection) uri.openConnection();
                         urlC.setRequestMethod("GET");
@@ -112,7 +105,7 @@ public class MainActivity extends AppCompatActivity implements Constants {
     }
 
 
-    private URL getUrl(String city) throws MalformedURLException {
+    protected static URL getUrl(String city) throws MalformedURLException {
         return new URL("https://api.openweathermap.org/data/2.5/weather?q="
                 + city + "&units=metric&appid=" + BuildConfig.WEATHER_API_KEY);
     }
@@ -123,14 +116,6 @@ public class MainActivity extends AppCompatActivity implements Constants {
         TextView Temp = findViewById(R.id.textWeather);
         TextView Pressure = findViewById(R.id.PressureStat);
         TextView Wind = findViewById(R.id.WindSpeed);
-
-        TextView currentCity = findViewById(R.id.currentCityV);
-        String temp = currentCity.getText().toString();
-
-        if (!wr.getName().equals(temp)) {
-            Toast.makeText(getApplicationContext(), "Неверный город", Toast.LENGTH_SHORT).show();
-        }
-
         Temp.setText(String.format("%f2", wr.getMain().getTemp()));
         Pressure.setText(String.format("%d", wr.getMain().getPressure()));
         Wind.setText(String.format("%d", wr.getWind().getSpeed()) + " M");
@@ -146,12 +131,12 @@ public class MainActivity extends AppCompatActivity implements Constants {
                 if (data != null) {
                     Parcel parcel = (Parcel) data.getSerializableExtra(PARCEL);
                     if (parcel != null) {
-                            currentCity.setText(parcel.currentCity);
-                            try {
-                                getUrl(String.valueOf(parcel));
-                            } catch (MalformedURLException e) {
-                                e.printStackTrace();
-                            }
+                        currentCity.setText(parcel.currentCity);
+                        try {
+                            getUrl(String.valueOf(parcel));
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
                     } else {
                         currentCity.setText(R.string.Default);
                     }
@@ -182,15 +167,15 @@ public class MainActivity extends AppCompatActivity implements Constants {
     }
 
 
-    public void onClick(View v) {
-        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-        startActivityForResult(intent, REQ_CODE_99);
-        Log.i(LOG, "OpenSecondActivity");
-    }
-
-
-    public void onRefresh(View v) {
-        Thread();
-        Log.i(LOG, "Thread Launch");
+    public void ButtonListener(View view) {
+        Button button = findViewById(R.id.buttonAdd);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivityForResult(intent, REQ_CODE_99);
+                Log.i(LOG, "OpenSecondActivity");
+            }
+        });
     }
 }
